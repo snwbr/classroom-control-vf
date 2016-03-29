85ace2578eb044bb481e950cd22f363019b66155
David Pérez <oso12_90@hotmail.com>
2016-03-29 11:12:04 -0600

    Create init.pp

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,10 @@
+class skeleton{
+  file{'/etc/skel':
+    ensure => 'directory',
+  }
+  
+  file{'/etc/skel/.bashrc':
+    ensure => 'present',
+    source  => 'puppet:///modules/skeleton/.bashrc',
+  }
+}
