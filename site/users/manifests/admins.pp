de51411a606687e6f7ec56c89c7e51ef726a490b
David Pérez <oso12_90@hotmail.com>
2016-03-30 14:35:13 -0600

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -2,4 +2,9 @@ class users::admins{
   users::managed_user { 'jose':}
   users::managed_user { 'alice':}
   users::managed_user { 'chen':}
+  
+  group { "admins":
+    ensure  => present,
+    gid     => "1234",
+  }
 }
