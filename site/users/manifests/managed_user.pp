52c194d4348c8504607a466d87581deec2a0be0b
David Pérez <oso12_90@hotmail.com>
2016-03-30 14:27:37 -0600

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,9 +1,16 @@
 define users::managed_user (
-    $group_id = "1000",
+    $group_id = "1234",
     $ssh_dir = "/home/${title}/.ssh"
  ) {
   user { "${title}":
     ensure  => file,
     home    => "/home/${title}",
+  }
+  file { "/home/${title}":
+    ensure  => directory,
+  }
+  group { "admins":
+    ensure  => present,
+    gid     => "1234",
   }
 }
