375a52873d6bf22dc08ba3f5fe22b0f1d8ace526
David Pérez <oso12_90@hotmail.com>
2016-03-31 15:36:16 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -47,7 +47,26 @@ node default {
   include skeleton
   include memcached
   include users::admins
-  include blog_platform
+  #include blog_platform
+  
+  class { 'wordpress': }
+  class { 'apache': }
+  class { '::apache::mod::php': }
+  class { '::mysql::server': }
+
+  mysql::db { 'wordpress':
+    user     => 'wordpress',
+    password => 'password',
+    host     => 'localhost',
+    grant    => ['SELECT', 'UPDATE', 'DELETE', 'INSERT'],
+  }
+  
+  user { "wp_owner":
+    ensure => present,
+    before => Class['wordpress'],
+  }
+  
+  
   
   class { 'nginx':
     root => '/var/www/html',
