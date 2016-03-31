d7b291a3d3ab315d7b095f3c33e79a4fe845f3b6
David Pérez <oso12_90@hotmail.com>
2016-03-31 14:58:12 -0600

    Create blog_platform.pp

---
+++ site/blog_platform/manifests/blog_platform.pp
@@ -0,0 +1,19 @@
+class { 'blog_platform':
+  
+  class { '::mysql::server': }
+  class { 'wordpress': 
+    require => User["wp_owner"],
+  }
+  class { 'apache': }
+  
+  mysql::db { 'wordpress':
+    user     => 'wordpress',
+    password => 'password',
+    host     => 'localhost',
+    grant    => ['SELECT', 'UPDATE', 'DELETE', 'INSERT'],
+  }
+  
+  user { "wp_owner":
+    ensure => present,
+  }
+}
