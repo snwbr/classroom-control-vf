5a550f08bff5077c3c2cd968e918169bae7c4bfe
David Pérez <oso12_90@hotmail.com>
2016-03-31 15:27:51 -0600

    Update init.pp

--- site/blog_platform/manifests/init.pp
+++ site/blog_platform/manifests/init.pp
@@ -1,20 +1,18 @@
-class { '::mysql::server': }
-class { '::mysql::db': }
-
-
-user { "wp_owner":
-  ensure => present,
-}
-
 class blog_platform {
 
   class { 'wordpress': require => User["wp_owner"],}
   class { 'apache': }
+  class { '::apache::mod::php': }
+  class { '::mysql::server': }
 
   mysql::db { 'wordpress':
     user     => 'wordpress',
     password => 'password',
     host     => 'localhost',
     grant    => ['SELECT', 'UPDATE', 'DELETE', 'INSERT'],
+  }
+  
+  user { "wp_owner":
+    ensure => present,
   }
 }
